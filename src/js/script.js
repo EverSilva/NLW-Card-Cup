bf340f067cbf2144cf9cff702b87a45dc76f8588
eversilva <everson.silva2706@gmail.com>
2022-10-30 14:38:40 -0300

    Change background body when card is clicked

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,20 @@ let isIgnite = true
 
 const changeCard = e => {
     const card = e.currentTarget
-    const backgroundImage = isIgnite  
+    const cardBackgroundImage = isIgnite  
     ? "url(src/assets/bg-ignite.svg)"
     : "url(src/assets/bg-explorer.svg)"
+    const bodyBackground = isIgnite ?
+    'linear-gradient(150deg, #185B34, #000000)' :
+    'linear-gradient(150deg, #373D98, #000000)'
+
     isIgnite = !isIgnite
-    changeBackground(card, backgroundImage)
+    changeBackground(card, cardBackgroundImage)
+    changeBodyBackground(bodyBackground)
 }
 
 const changeBackground = (element, backgroundImage) => 
     element.style.backgroundImage = backgroundImage
+
+const changeBodyBackground = background => 
+    document.body.style.backgroundImage = background
