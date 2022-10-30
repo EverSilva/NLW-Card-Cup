c2112d24d5d2c9beb0fd2c25570518350f758b74
eversilva <everson.silva2706@gmail.com>
2022-10-30 14:55:37 -0300

    Adjust changeCard function order

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ let isIgnite = true
 
 const changeCard = e => {
     const card = e.currentTarget
+    isIgnite = !isIgnite
     const cardBackgroundImage = isIgnite  
     ? "url(src/assets/bg-ignite.svg)"
     : "url(src/assets/bg-explorer.svg)"
@@ -9,7 +10,6 @@ const changeCard = e => {
     'linear-gradient(150deg, #185B34, #000000)' :
     'linear-gradient(150deg, #373D98, #000000)'
 
-    isIgnite = !isIgnite
     changeBackground(card, cardBackgroundImage)
     changeBodyBackground(bodyBackground)
 }
